fee92522d69b62ead6ff1307e60d753c68f847f4
Ackerman051212 <2285408058@qq.com>
2026-02-05 20:18:49 +0800

    Add files via upload
    c语言初阶 分支与循环语句的进一步学习

--- FileName.cpp
+++ FileName.cpp
@@ -297,34 +297,366 @@
 //结构体
 
 
-#include<stdio.h>
+//#include<stdio.h>
 
 
 //学生
 
-struct Stu
-{
-	//成员
-	char name[20];
-	int age;
-	char sex[10];
-	char tele[12];
-};
+//struct Stu
+//{
+//	//成员
+//	char name[20];
+//	int age;
+//	char sex[10];
+//	char tele[12];
+//};
+//
+//void print(struct Stu* ps)
+//{
+//	printf("%s %d %s %s\n", (*ps).name, (*ps).age, (*ps).sex, (*ps).tele);
+//
+//	//->
+//	//结构体指针变量->成员名
+//	printf("%s %d %s %s\n", ps->name, ps->age, ps->sex, ps->tele);
+//}
+//int main()
+//{
+//	struct Stu s = {"zhangsan",20,"nan","13794998455"};//初始化
+//
+//	//结构体对象.成员名
+//	printf("%s %d %s %s\n", s.name, s.age, s.sex, s.tele);
+//	print(&s);
+//	return 0;
+//}
 
-void print(struct Stu* ps)
-{
-	printf("%s %d %s %s\n", (*ps).name, (*ps).age, (*ps).sex, (*ps).tele);
 
-	//->
-	//结构体指针变量->成员名
-	printf("%s %d %s %s\n", ps->name, ps->age, ps->sex, ps->tele);
-}
+
+//#include<stdio.h>
+//
+//typedef struct student
+//{
+//	int height;
+//	char name[20];
+//
+//}stu;
+//
+//
+//
+//
+//int main()
+//{
+//	stu stu = { 170,"张三" };
+//	
+//
+//	printf("height=%d,name=%s", stu.height, stu.name);
+//
+//	return 0;
+//}
+
+
+
+
+
+
+//#include <stdio.h>
+//
+//
+//int main()
+//{
+	//int a = 10;
+	//if (a = 3)//a非零为真
+	//	printf("yes");
+
+	//int age = 0;
+	//if (age < 18)
+	//{
+	//	printf("未成年\n");
+	//	printf("不能喝酒\n");
+	//}
+	//	
+	//else
+	//	printf("成年\n");
+//	printf("请输入年龄\n");
+//	scanf("%d", &age);
+//
+//	if (age < 18)
+//	{
+//		printf("未成年\n");
+//	}
+//	else if (age >= 18 && age < 28)
+//	{
+//		printf("青年\n");
+//	}
+//	else if (age >= 28 && age < 40)
+//	{
+//		printf("中年\n");
+//	}
+//	else if (age >= 40 && age < 60)
+//	{
+//		printf("壮年\n");
+//	}
+//	else if (age >= 60 && age <= 100)
+//	{
+//		printf("老年\n");
+//	}
+//	else
+//		printf("老寿星\n");
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+#include<stdio.h>
+
+//int test()
+//{
+//	int a = 6;
+//		if (a == 3)
+//			return 1;
+//		else
+//			return 0;
+//}
+//
+//
+//int main()
+//{
+//	int r = test();
+//		printf("%d", r);
+//
+//	return 0;
+//}
+
+//判断一个数是否为奇数
+
+//int main()
+//{
+//	int num = 0;
+//	printf("请输入一个数:");
+//	scanf("%d", &num);
+//	if (0 == num % 2)
+//	{
+//		printf("这个数不是奇数是偶数");
+//	}
+//	else
+//	{
+//		printf("这个数是奇数");
+//	}
+//	return 0;
+//}
+
+
+
+
+//输出1到100之间的奇数
+
+
+//int main()
+//{
+//	int num = 0;
+//	while (num < 100)
+//	{
+//		num++;
+//		if (0 != num % 2)
+//		{
+//			printf("%d\n", num);
+//		}
+//		else
+//			;
+//	}
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int day = 0;
+//	scanf("%d", &day);
+//	switch (day)
+//	{
+//	case 1:
+//	case 2:
+//	case 3:
+//	case 4:
+//	case 5:
+//		printf("weekday");
+//		break;
+//	case 6:
+//	case 7:
+//		printf("weekend\n");
+//		break;
+//	default:
+//		printf("选择错误!");
+//		break;
+//	}
+//
+//	return 0;
+//}
+
+
+//打印1~10的数字
+
+//while循环
+//break的作用是永久地终止循环
+//continue的作用是跳过本次循环后面的代码,直接去判断部分，进行下一次循环的判断
+
+
+
+//int main()
+//{
+//	int i = 1;
+//	while(i<=10)
+//	{
+//		i++;
+//		if (i == 5)
+//		{
+//			continue;
+//		}
+//		printf("%d ", i);
+//	}
+//
+//	return 0;
+//}
+
+
+//密码
+//int main()
+//{
+//	char password[20];
+//	printf("请输入密码:");
+//	scanf("%s", password);
+//	//清理输入缓冲区
+//	//getchar();//读取了\n
+//	//如果abcdef xxxx\n有这样一系列的字符串需要清理
+//	int ch = 0;
+//	while ((ch = getchar()) != '\n')
+//	{
+//		;
+//	}
+//	printf("请确认密码(Y/N):>");
+//	int ret = getchar();
+//	if ('Y' == ret)
+//		printf("Yes\n");
+//	else
+//		printf("No\n");
+//
+//	return 0;
+//}
+
+
+
+
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1;i <= 10;i++)
+//	{
+//		if (5 == i)
+//			continue;
+//		printf("%d ", i);
+//	}
+//
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int i = 0;
+//	int j = 0;
+//	for (i = 0;i < 10;i++)
+//	{
+//		for (j = 0;j < 10;j++)
+//		{
+//			printf("ok\n");
+//		}
+//	}
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int i = 1;
+//	do
+//	{
+//		if (5 == i)
+//			break;
+//		printf("%d ", i);
+//		i++;
+//	} 
+//	while (i <= 10);
+//
+//	return 0;
+//}
+
+
+
+//计算n的阶乘
+//1*2*3*4*...*n
+
+
+//int main()
+//{
+//	int n = 0;
+//	int i = 1;
+//	int ret = 1;
+//	printf("请输入n的值:");
+//	scanf("%d", &n);
+//	for (i = 1;i <= n;i++)
+//	{
+//		ret=ret*i;
+//	}
+//	printf("%d\n", ret);
+//	return 0;
+//}
+
+
+
+
+//计算1!+2!+3!+...+10!
+
+//int main()
+//{
+//	int i = 0;
+//	int sum = 0;
+//	int ret = 1;
+//	for (i = 1;i <= 10;i++)
+//	{
+//		ret = ret * i;
+//		sum = sum + ret;
+//	}
+//	printf("%d\n", sum);
+//	return 0;
+//}
+
+
+
+
+
 int main()
 {
-	struct Stu s = {"zhangsan",20,"nan","13794998455"};//初始化
+	int i = 0;
+	int ret = 1;
+	int sum = 0;
+	int n = 0;
+	for (n = 1;n <= 10;n++)
+	{
+		ret = 1;
+		for (i = 1;i <= n;i++)
+		{
+			ret = ret * i;
+		}
+		sum = sum + ret;
+	}
+	
+	printf("%d\n", sum);
 
-	//结构体对象.成员名
-	printf("%s %d %s %s\n", s.name, s.age, s.sex, s.tele);
-	print(&s);
 	return 0;
 }
